18ead29fd0d58662b3eb09e97cd3a67ddb6f8fd0
Thiago Oliveira <thiceconelo@gmail.com>
2023-07-11 16:19:17 -0300

    Build public: languages content

---
+++ index.search.js
@@ -0,0 +1,44 @@
+var relearn_search_index = [
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Javascript",
+    "uri": "/linguagens/javascript/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Linguagens",
+    "uri": "/linguagens/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Python",
+    "uri": "/linguagens/python/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Sonath - Developer Guide ",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
