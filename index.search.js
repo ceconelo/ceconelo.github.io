bfc2b7f5eeed513e1f51d18c040710339443c900
Thiago Oliveira <thiceconelo@gmail.com>
2023-07-11 23:49:16 -0300

    About project

--- index.search.js
+++ index.search.js
@@ -7,7 +7,7 @@ var relearn_search_index = [
     "uri": "/linguagens/javascript/index.html"
   },
   {
-    "content": "Lorem Ipsum.\n",
+    "content": " Javascript Python ",
     "description": "",
     "tags": null,
     "title": "Linguagens",
@@ -28,10 +28,10 @@ var relearn_search_index = [
     "uri": "/categories/index.html"
   },
   {
-    "content": "Lorem Ipsum.\n",
+    "content": "Bem vindo ! Seja bem vindo ao meu espaço, ou melhor, ao nosso espaço. Sim, foi proposital primeiramente o uso do pronome possessivo meu e depois o nosso. Isso porque em um primeiro momento percebi que precisa armazenar o conhecimento que estava adquirindo, servindo como uma espécie de diário mas depois percebi que este conhecimento teria um valor ainda maior se fosse compartilhado, e é isso que quero fazer aqui.\nAlém do mais sei que muitas pessoas tem dificuldades em encontrar conteúdo de qualidade na internet, principalmente em português. Então chega mais, sinta-se a vontade e aproveite o conteúdo que estou disponibilizando aqui.\nQuero que fique claro que não me considero o melhor do mundo nos diversos assuntos que vou abordar aqui, mas acredito que posso ajudar muitas pessoas com o que sei e com os desafios que já enfrentei e que ainda por vezes enfrento.\nSobre mim Meu nome é Thiago Oliveira, estudande de Ciência da Computação, apaixonado por tecnologia e pelos desafios que esse mundo me proporciona. Atualmente ( 13 anos…hehe ) tenho uma empresa Cditec que presta serviços de desenvolvimento de software, consultoria, admnistração de redes, segurança da informação e infraestrutura de TI.\nNos últimos anos tenho me dedicado à projetos que envolvem WebScraping, RPA, Integração de Sistemas e Desenvolvimento de APIs e engenharia reversa de sistemas web.\nNeste sentido me envolvi com diversas tecnologias como: Python, Javascript, NodeJS, Docker, Selenium, Puppeteer, Scrapy, BeautifulSoup, FastAPI, VueJS, entre outras.\nO que você vai encontrar aqui Como já deve ter percebido me envolvo com diversas tecnologias e assuntos, então aqui você vai encontrar conteúdos sobre:\nWebScraping RPA Integração de Sistemas Desenvolvimento de APIs Engenharia Reversa Desenvolvimento de Software DevOps Padrões de Projeto Arquitetura de Software Segurança da Informação Docker Linux Github Como mencionei anteriormente, uso este espaço como um diário, isso significa que se as coisas por aqui estiverem muito bagunçadas é porque estou aprendendo e ainda não tive tempo de organizar tudo. Mas não se preocupe, com o tempo tudo vai ficar mais organizado e com mais conteúdo.\nCaso tenha alguma dúvida, sugestão ou crítica, fique a vontade para entrar em contato comigo através do meu Linkedin\n",
     "description": "",
     "tags": null,
-    "title": "Sonath - Developer Guide ",
+    "title": "Sonath - Base de Conhecimento",
     "uri": "/index.html"
   },
   {
